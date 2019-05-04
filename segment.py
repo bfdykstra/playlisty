3c379488f658ad5b199fc8e96f98eef3a897a088
Ben Dykstra <bfdykstra@email.wm.edu>
2019-05-04 14:16:27 -0400

    add docstrings to modules

--- segment.py
+++ segment.py
@@ -1,13 +1,19 @@
 import numpy as np
 import librosa
 
-"""
+
+def segment_onset(signal, sr=22050, hop_length=512, backtrack=True):
+    """
     Segment a signal using onset detection
+    Parameters:
+        signal: numpy array of a timeseries of an audio file
+        sr: int, sampling rate, default 22050 samples per a second
+        hop_length: number of samples between successive frames
+        backtrack: bool, If True, detected onset events are backtracked to the nearest preceding minimum of energy
     returns:
         dictionary with attributes segemented and shape
         
-"""
-def segment_onset(signal, sr, hop_length, backtrack=True):
+    """
     # Compute the frame indices for estimated onsets in a signal
     onset_samples = librosa.onset.onset_detect(signal, sr=sr, hop_length=hop_length, backtrack=backtrack, units='samples')
     
