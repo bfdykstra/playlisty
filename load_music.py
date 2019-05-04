61c6875b6fff65a08c68306fa4e0cc630f8b696f
Ben Dykstra <bfdykstra@email.wm.edu>
2019-05-04 13:52:08 -0400

    implemented load_music, segmentation returns audio samples, not frames

--- load_music.py
+++ load_music.py
@@ -1,5 +1,13 @@
 import librosa
 
-def load_music_dir(directory):
+
+def get_song_names(directory):
     # librosa has utility methods that find readable file formats given a directory
-    return
+    return librosa.util.find_files(directory)
+
+def load_song(file_path):
+    try:
+        signal, sr = librosa.load(file_path)
+        return (signal, sr)
+    except Exception as e:
+        print "Error loading {}: {}".format(file_path, e) 
--- segment.py
+++ segment.py
@@ -4,24 +4,25 @@ import librosa
 """
     Segment a signal using onset detection
     returns:
-        dictionary with attributes segemtned and shape
+        dictionary with attributes segemented and shape
         
 """
 def segment_onset(signal, sr, hop_length, backtrack=True):
     # Compute the frame indices for estimated onsets in a signal
-    onset_frames = librosa.onset.onset_detect(x, sr=sr, hop_length=hop_length, backtrack=backtrack)
+    onset_samples = librosa.onset.onset_detect(signal, sr=sr, hop_length=hop_length, backtrack=backtrack, units='samples')
     
-    # return np array of segments
+    # return np array of audio segments, within each segment is the actual audio data
     prev_ndx = 0
     segmented = []
-    for frame in onset_frames:
-        segmented.append(np.array(x[prev_ndx:frame]))
+    for frame in onset_samples:
+        segmented.append(np.array(signal[prev_ndx:frame]))
         prev_ndx = frame
     return {'segmented': np.array(segmented), 'shape': np.array(segmented).shape}
-
+    
 # return a np array of shape (num_segments, # of hops that fit in entire signal)
 def segment(signal, num_segments=1024, hop_length=512):
     frames = librosa.util.frame(signal, frame_length=num_segments, hop_length=hop_length)
     
     return frames
+
     
